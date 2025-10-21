323581770a086e38c9fa64232a31229cd1f4dcea
Your Name <your.email@example.com>
2025-10-21 18:29:22 +0900

    Fix aws key

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_REGION = process.env.AWS_REGION;
+
 const APP_PORT = process.env.SERVER_PORT || 8080;
 
 const dbConfig = {
